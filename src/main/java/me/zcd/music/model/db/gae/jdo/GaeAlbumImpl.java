3078bd567c220948f70d4e2f58e69ce55d46bd32
mikehershey <mikehershey32@gmail.com>
2011-07-21 19:01:09 -0400

    license info

--- src/main/java/me/zcd/music/model/db/gae/jdo/GaeAlbumImpl.java
+++ src/main/java/me/zcd/music/model/db/gae/jdo/GaeAlbumImpl.java
@@ -1,3 +1,12 @@
+/**
+ * Copyright © 2011 Mike Hershey (http://mikehershey.com | http://zcd.me) 
+ * 
+ * See the LICENSE file included with this project for full permissions. If you
+ * did not receive a copy of the license email mikehershey32@gmail.com for a copy.
+ * 
+ * Among other restrictions you are not permitted to deploy this software for 
+ * commercial purposes.
+ */
 package me.zcd.music.model.db.gae.jdo;
 
 import java.util.Date;
